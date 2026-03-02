97c5a38140321ba7f6bf689548fc5de87b189520
aditik06 <kashyapaditi593@gmail.com>
2026-03-02 22:37:35 +0530

    UC4 - Character array based palindrome check implemented

---
+++ UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,42 @@
+/**
+ * Use Case 4
+ * Character Array Based Palindrome Check
+ * Author: Lakshmi M
+ * Version: 1.0
+ */
+
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original String
+        String word = "radar";
+
+        // Convert String to Character Array
+        char[] characters = word.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
