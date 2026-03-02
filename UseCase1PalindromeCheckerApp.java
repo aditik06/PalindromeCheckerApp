990121ddc56820c83d4903d6e8e98d906e336b94
aditik06 <kashyapaditi593@gmail.com>
2026-03-02 22:29:54 +0530

    Initial commit - UC1 Application Entry implemented

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+/**
+ * Use Case 1
+ * Application Entry & Welcome Message
+ * Author: Lakshmi M
+ * Version: 1.0
+ */
+
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Application Name
+        System.out.println("===========================================");
+        System.out.println("        PALINDROME CHECKER APPLICATION     ");
+        System.out.println("===========================================");
+
+        // Application Version
+        System.out.println("Version: 1.0.0");
+
+        // Description
+        System.out.println("This application checks whether a word");
+        System.out.println("or sentence is a palindrome.");
+
+        System.out.println("===========================================");
+        System.out.println("Application started successfully!");
+        System.out.println("===========================================");
+        System.out.println("Ready to proceed to next use case...");
+    }
+}
